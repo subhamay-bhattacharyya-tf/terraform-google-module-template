04a067512e0d3784341ee1d23b10d499c2e25c90
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2026-03-26 13:36:30 -0400

    refactor: restructure GCS bucket module by removing unnecessary module wrapper and updating output references

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,43 @@
 # ============================================================================
-# GCS Bucket - Outputs
+# GCS Bucket Module - Outputs
 # ============================================================================
 
 output "bucket_id" {
   description = "The ID of the GCS bucket."
-  value       = module.gcs_bucket.bucket_id
+  value       = google_storage_bucket.this.id
 }
 
 output "bucket_name" {
   description = "The name of the GCS bucket."
-  value       = module.gcs_bucket.bucket_name
+  value       = google_storage_bucket.this.name
 }
 
 output "bucket_project" {
   description = "The project ID where the bucket is created."
-  value       = module.gcs_bucket.bucket_project
+  value       = google_storage_bucket.this.project
 }
 
 output "bucket_location" {
   description = "The location of the GCS bucket."
-  value       = module.gcs_bucket.bucket_location
+  value       = google_storage_bucket.this.location
 }
 
 output "bucket_url" {
   description = "The URL of the GCS bucket."
-  value       = module.gcs_bucket.bucket_url
+  value       = google_storage_bucket.this.url
 }
 
 output "bucket_self_link" {
   description = "The self link of the GCS bucket resource."
-  value       = module.gcs_bucket.bucket_self_link
+  value       = google_storage_bucket.this.self_link
 }
 
 output "bucket_storage_class" {
   description = "The storage class of the GCS bucket."
-  value       = module.gcs_bucket.bucket_storage_class
+  value       = google_storage_bucket.this.storage_class
 }
 
 output "bucket_force_destroy" {
   description = "Whether force_destroy is enabled for the GCS bucket."
-  value       = module.gcs_bucket.bucket_force_destroy
+  value       = google_storage_bucket.this.force_destroy
 }

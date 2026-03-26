0c8053eb01d0911cb958d01464d42b29d184f922
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2026-03-26 13:24:39 -0400

    style: format spacing in GCS bucket example configuration

--- examples/bucket/basic/main.tf
+++ examples/bucket/basic/main.tf
@@ -6,9 +6,9 @@
 module "gcs_bucket" {
   source = "../../.."
 
-  bucket_name = "my-portfolio-bucket"
-  project_id  = "portfolio-site"
-  location    = "US"
+  bucket_name   = "my-portfolio-bucket"
+  project_id    = "portfolio-site"
+  location      = "US"
   storage_class = "STANDARD"
   force_destroy = false
   versioning    = false
